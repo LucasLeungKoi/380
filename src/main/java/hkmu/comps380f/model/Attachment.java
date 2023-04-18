fa0dd88a09b9945240c8736d2cf0d36a9efe0cbd
LucasLeungKoi <104263817+LucasLeungKoi@users.noreply.github.com>
2023-04-19 03:22:27 +0800

    Task 1 done

--- src/main/java/hkmu/comps380f/model/Attachment.java
+++ src/main/java/hkmu/comps380f/model/Attachment.java
@@ -1,24 +1,46 @@
 package hkmu.comps380f.model;
 
+import jakarta.persistence.*;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.util.UUID;
+
+@Entity
 public class Attachment {
-    private String id;
+    @Id
+    @GeneratedValue
+    @ColumnDefault("random_uuid()")
+    private UUID id;
+
+    @Column(name = "filename")
     private String name;
+
+    @Column(name = "content_type")
     private String mimeContentType;
+
+    @Column(name = "content")
+    @Basic(fetch = FetchType.LAZY)
+    @Lob
     private byte[] contents;
 
-    // Getters and Setters of id, name, mimeContentType, contents
-    public String getId() {
+    @Column(name = "ticket_id", insertable=false, updatable=false)
+    private long ticketId;
+
+    @ManyToOne
+    @JoinColumn(name = "ticket_id")
+    private Ticket ticket;
+
+    // getters and setters of all properties
+    public UUID getId() {
         return id;
     }
-
-    public void setId(String id) {
+    public void setId(UUID id) {
         this.id = id;
     }
 
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
@@ -26,7 +48,6 @@ public class Attachment {
     public String getMimeContentType() {
         return mimeContentType;
     }
-
     public void setMimeContentType(String mimeContentType) {
         this.mimeContentType = mimeContentType;
     }
@@ -34,10 +55,21 @@ public class Attachment {
     public byte[] getContents() {
         return contents;
     }
-
     public void setContents(byte[] contents) {
         this.contents = contents;
     }
 
+    public long getTicketId() {
+        return ticketId;
+    }
+    public void setTicketId(long ticketId) {
+        this.ticketId = ticketId;
+    }
 
+    public Ticket getTicket() {
+        return ticket;
+    }
+    public void setTicket(Ticket ticket) {
+        this.ticket = ticket;
+    }
 }
